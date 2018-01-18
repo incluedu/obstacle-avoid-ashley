0ed38f63727ad686d2e9f5e61b12921ed8c9a76f
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2018-01-18 21:31:19 +0100

    after lesion 132

--- core/src/net/lustenauer/obstacleavoid/screen/game/GameScreen.java
+++ core/src/net/lustenauer/obstacleavoid/screen/game/GameScreen.java
@@ -1,10 +1,17 @@
 package net.lustenauer.obstacleavoid.screen.game;
 
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Logger;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import net.lustenauer.obstacleavoid.ObstacleAvoidGame;
+import net.lustenauer.obstacleavoid.config.GameConfig;
 import net.lustenauer.obstacleavoid.screen.menu.MenuScreen;
+import net.lustenauer.obstacleavoid.system.debug.GridRenderSystem;
+import net.lustenauer.obstacleavoid.util.GdxUtils;
 
 /**
  * @author Patric Hollenstein
@@ -15,6 +22,10 @@ public class GameScreen implements Screen {
     private final ObstacleAvoidGame game;
     private final AssetManager assetManager;
 
+    private Viewport viewport;
+    private ShapeRenderer renderer;
+    private PooledEngine engine;
+
 
     public GameScreen(ObstacleAvoidGame game) {
         this.game = game;
@@ -23,17 +34,23 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
+        log.debug("show");
+        viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT);
+        renderer = new ShapeRenderer();
+        engine = new PooledEngine();
 
+        engine.addSystem(new GridRenderSystem(viewport, renderer));
     }
 
     @Override
     public void render(float delta) {
-
+        GdxUtils.clearScreen();
+        engine.update(delta);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -53,6 +70,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        renderer.dispose();
     }
 }
